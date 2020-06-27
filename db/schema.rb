ca47ceff4481536ae159143b2414567944eeaae3
yuichiyasui <yassan.b.a.w@gmail.com>
2020-06-27 19:32:49 +0900

    第2章完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_152502) do
+ActiveRecord::Schema.define(version: 2020_06_27_093717) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
